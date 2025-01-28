d02e6510d394e2d9ccd363b12e9f151fce8537b8
HFooladi <fooladi.hosein@gmail.com>
2025-01-28 18:47:23 +0100

    add github workflow folder

--- moltoken/tokenizer/mol_tokenizer.py
+++ moltoken/tokenizer/mol_tokenizer.py
@@ -26,6 +26,8 @@ class MoleculeTokenizer(Tokenizer):
         self.spatial_resolution = spatial_resolution
         self.consider_bonds = consider_bonds
         self.atom_vocabulary = {}  # Will store atom type mappings
+        self.bond_vocabulary = {}  # Will store bond type mappings
+        self.special_tokens = {}
         
     def _process_3d_structure(
         self,
@@ -65,7 +67,29 @@ class MoleculeTokenizer(Tokenizer):
             List of integer tokens representing the molecule
         """
         # Implementation needed: Convert molecule to token sequence
-        raise NotImplementedError
+        tokens = [self.special_tokens['START']]
+        
+        # Discretize coordinates
+        disc_coords = self._discretize_coordinates(coordinates)
+        
+        # Encode atoms and their positions
+        for atom_type, coord in zip(atom_types, disc_coords):
+            # Add atom type token
+            atom_token = self.atom_vocab.get(atom_type, len(self.atom_vocab))  # Unknown atoms get new tokens
+            tokens.append(atom_token + 259)  # Offset to avoid special tokens
+            
+            # Add discretized coordinate tokens
+            for c in coord:
+                tokens.append(int(c) + 300)  # Offset for coordinate tokens
+        
+        # Encode bonds if available
+        if bonds and self.consider_bonds:
+            for start_idx, end_idx, bond_type in bonds:
+                bond_token = self.bond_vocab.get(bond_type, len(self.bond_vocab))
+                tokens.append(bond_token + 400)  # Offset for bond tokens
+        
+        tokens.append(self.special_tokens['END'])
+        return tokens
         
     def decode_to_molecule(
         self,
@@ -81,7 +105,47 @@ class MoleculeTokenizer(Tokenizer):
             Tuple of (coordinates, atom_types, bonds)
         """
         # Implementation needed: Convert tokens back to molecular representation
-        raise NotImplementedError
+        # Remove START and END tokens
+        tokens = tokens[1:-1]
+        
+        # Initialize lists for molecular components
+        coordinates = []
+        atom_types = []
+        bonds = []
+        
+        # Reverse vocabularies for decoding
+        rev_atom_vocab = {v: k for k, v in self.atom_vocab.items()}
+        rev_bond_vocab = {v: k for k, v in self.bond_vocab.items()}
+        
+        i = 0
+        while i < len(tokens):
+            # Decode atom
+            if 259 <= tokens[i] < 300:  # Atom token range
+                atom_idx = tokens[i] - 259
+                atom_types.append(rev_atom_vocab.get(atom_idx, 'Unknown'))
+                
+                # Get coordinates
+                if i + 3 < len(tokens):
+                    coord = [
+                        (tokens[i+1] - 300) * self.spatial_resolution,
+                        (tokens[i+2] - 300) * self.spatial_resolution,
+                        (tokens[i+3] - 300) * self.spatial_resolution
+                    ]
+                    coordinates.append(coord)
+                    i += 4
+                else:
+                    break
+            
+            # Decode bonds
+            elif tokens[i] >= 400:  # Bond token range
+                bond_idx = tokens[i] - 400
+                if len(bonds) > 0:  # Add bond information
+                    bonds.append((len(atom_types)-2, len(atom_types)-1, rev_bond_vocab.get(bond_idx, 1.0)))
+                i += 1
+            else:
+                i += 1
+        
+        return np.array(coordinates), atom_types, bonds
 
 
     
